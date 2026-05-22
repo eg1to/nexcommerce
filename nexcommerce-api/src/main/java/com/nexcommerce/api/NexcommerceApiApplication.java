cf0b49a2e43078f418ec129d712d6e65d2d977eb
eg1to <pedro.h.egito@gmail.com>
2026-05-22 16:24:18 -0300

    Add initial project structure with Maven setup and PostgreSQL configuration

---
+++ nexcommerce-api/src/main/java/com/nexcommerce/api/NexcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.nexcommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexcommerceApiApplication.class, args);
+	}
+
+}
---
+++ nexcommerce-api/src/test/java/com/nexcommerce/api/NexcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexcommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
