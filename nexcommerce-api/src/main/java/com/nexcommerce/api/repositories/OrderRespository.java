bf8a7f0fbe8fdc7d5ab53524f421a86087e5d07c
eg1to <pedro.h.egito@gmail.com>
2026-05-24 10:12:56 -0300

    Update AddressRepository to use UUID and add OrderRepository for Order entity persistence

--- nexcommerce-api/src/main/java/com/nexcommerce/api/repositories/OrderRespository.java
+++ nexcommerce-api/src/main/java/com/nexcommerce/api/repositories/OrderRespository.java
@@ -2,10 +2,10 @@ package com.nexcommerce.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nexcommerce.api.entities.Address;
+import com.nexcommerce.api.entities.Order;
 
 import java.util.UUID;
 
-public interface AddressRepository extends JpaRepository<Address, UUID> {
+public interface OrderRespository extends JpaRepository<Order, UUID> {
     
 }
